ebe75dcbb74cb838c8f1bf7791c1a3f32246dcb0
sowmya-mula <sowmya3950@gmail.com>
2026-06-06 19:07:29 +0530

    Update ddl.bronze.sql

--- scripts/bronze/ddl.bronze.sql
+++ scripts/bronze/ddl.bronze.sql
@@ -32,8 +32,8 @@ CREATE TABLE bronze.crm_cust_info (
 cst_id INT,
 cst_key NVARCHAR(50),
 cst_firstname NVARCHAR(50),
-cst_lasttname NVARCHAR(50),
-cst_material_status NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_martial_status NVARCHAR(50),
 cst_gndr NVARCHAR(50),
 cst_create_date DATE
 );
